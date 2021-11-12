c6a9871f92b44bc4014288538bb2d176523d5f10
ftrr <ftrrbnnn@gmail.com>
2021-11-12 15:50:04 +0100

    Sprint 1 - Tema 4 POO - Nivell 1 - Exercici 2

--- nivell1/index.php
+++ nivell1/index.php
@@ -1,5 +1,7 @@
 <?php
 
+echo "<br><br><b>Exercici 1</b><br>";
+
 class Employee {
 
     //Atributs
@@ -23,3 +25,47 @@ class Employee {
 $empleat1 = new Employee();
 $empleat1 -> initialize('Joan',30000);
 $empleat1 -> print();
+
+
+echo "<br><br><b>Exercici 2</b><br>";
+
+class Shape{
+
+    //Atributos
+    protected $amplada;
+    protected $altura;
+    protected $area;
+
+    //Métodos
+    public function __construct($amp,$alt){
+        $this->amplada=$amp;
+        $this->altura=$alt;
+    }
+
+    public function imprimir(){
+        echo $this -> area . "<br>";
+    }
+
+}
+
+class Triangle extends Shape{
+    public function area(){
+        $this->area = $this->amplada*$this->altura;
+    }
+}
+
+class Rectangle extends Shape{
+    public function area(){
+        $this->area = $this->amplada*$this->altura;
+    }
+}
+
+$triangle = new Triangle(10,20);
+$triangle -> area();
+echo "L'àrea del triangle és: ";
+$triangle-> imprimir();
+
+$rectangle = new Rectangle(20,20);
+$rectangle -> area();
+echo "L'àrea del rectangle és: ";
+$rectangle-> imprimir();
